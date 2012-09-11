5bf3dfce3f60345a6648690f3d225aeecbe61566
Alex John <alex.ksiazek@gmail.com>
2012-09-11 14:18:08 -0500

    some more work on p3

--- p3.c
+++ p3.c
@@ -1,22 +1,23 @@
 /*
  * Problem 3
  *
- * What is the largest prime factor of the number 600851475143?
+ * What is the largest prime factor of the number 600,851,475,143?
  */
 
 #include <stdio.h>
 
 int main ()
 {
-    long long n = 600851475143;
-    long long lp;
-    int       i;
-    for (i = 1; i < n; ++i) {
-        if (!(n%i)) {
-            printf ("%d\n", i);
+    float x = 600851475143;
+    float n = (x/2)+1;
+    float lp;
+    float i;
+    for (i=1; i<n; ++i) {
+        if (!(x%i)) {
+            printf ("%f\n", i);
             lp = i;
         }
     }
-    printf ("The largest prime of %d is %d\n", n, lp);
+    printf ("The largest prime of %f is %f\n", x, lp);
     return 0;
 }
